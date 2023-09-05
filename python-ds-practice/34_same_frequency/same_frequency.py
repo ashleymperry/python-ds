ebd168a970e6bd250020970f670a390413f27d7f
ashleymperry <123785437+ashleymperry@users.noreply.github.com>
2023-09-05 06:37:45 -0400

    Add files via upload

---
+++ python-ds-practice/34_same_frequency/same_frequency.py
@@ -0,0 +1,20 @@
+def frequency(coll):
+    count = {}
+    for x in coll:
+        count[x] = count.get(x, 0) + 1
+
+    return count
+
+def same_frequency(num1, num2):
+    """Do these nums have same frequencies of digits?
+    
+        >>> same_frequency(551122, 221515)
+        True
+        
+        >>> same_frequency(321142, 3212215)
+        False
+        
+        >>> same_frequency(1212, 2211)
+        True
+    """
+    return frequency(str(num1)) == frequency(str(num2))
